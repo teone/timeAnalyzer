408a5e8a11058ac9e4fadb18afa96e9ff1b12398
teone <teo.punto@gmail.com>
2014-10-15 19:24:50 +0200

    Cleaned code

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -23,36 +23,53 @@ angular.module('timeAnalyzerApp')
 
         $scope.totalProject = {};
 
+        //group the entry by project
         var groupedProject = _.groupBy(formattedRes, 'PROJECT');
 
+        //cicle the project
         _.forEach(groupedProject, function(project){
 
+            //assign the label
             var projectLabel = project[0].PROJECT;
+
+            //define an empty var for this Project total time
             var projectTotalTime = 0;
+
+            //group the project's task
             var groupedTask = _.groupBy(project, 'TASK');
 
-            //calculate the total splitted by task
+            //define an object to store the task total
             var totalTask = {};
+
+            //define an empty var for the counter of task total time
             var totalTaskTime;
+
+            //cicle the Project's group of tasks
             _.forEach(groupedTask, function(taskGroup){
+
+                //assign a label to the task
                 var label = taskGroup[0].TASK;
+
+                //resetting the calculated time for this task
                 totalTaskTime = 0;
+
+                //cicle each entry of a task group
                 _.forEach(taskGroup, function(task){
+
                     if(task.HOURS){
+                        //update the task total
                         totalTaskTime += parseFloat(task.HOURS.replace(',','.'), 10);
                     }
                 });
+                //assign the total time worked on a task to that task
                 totalTask[label] = totalTaskTime;
+                console.log(totalTask);
+                //incremente the total worked
+                grandTotal += totalTaskTime;
             });
 
+            //assign to task group object to the project object
             $scope.totalProject[projectLabel] = totalTask;
-            projectTotalTime += totalTaskTime; 
-            $scope.totalProject[projectLabel].total = projectTotalTime;
-
-            //calculate the total hours tracked
-            _.forEach($scope.totalTask, function(groupTotal){
-                grandTotal += groupTotal;
-            });
         });
 
         console.log($scope.totalProject);
